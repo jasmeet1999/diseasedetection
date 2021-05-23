c4d431b9689f410c4db55dc9aaefc9ca21356be6
Jasmeet Singh Khokhar <jasmeetsinghkhokhar@gmail.com>
2021-05-23 16:25:08 +0530

    resolve css link error

--- classifier/routes.py
+++ classifier/routes.py
@@ -1,8 +1,12 @@
 from classifier import app
-from flask import render_template
+from flask import render_template, redirect, url_for
 
 
 @app.route("/")
+def to_home():
+    return redirect(url_for("home"))
+
+
 @app.route("/index")
 def home():
     return render_template("index.html")
