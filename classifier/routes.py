b728189eea0decf2b166102f8f6b3562f50d1326
Jasmeet Singh Khokhar <jasmeetsinghkhokhar@gmail.com>
2021-05-26 12:55:51 +0530

    add predict page (output)

--- classifier/routes.py
+++ classifier/routes.py
@@ -17,6 +17,12 @@ def home():
 @app.route("/preview", methods=["GET", "POST"])
 def preview():
     if request.method == "POST":
+        # Deleting old files
+        rm_path = "classifier/static/user_file"
+        lists = os.listdir(os.path.join(os.getcwd(), rm_path))
+        print(lists)
+        for img in lists:
+            os.remove(os.path.join(rm_path, img))
         file = request.files["file"]
         file_name = secure_filename(file.filename)
         file_path = "classifier/static/user_file/" + file_name
@@ -24,3 +30,13 @@ def preview():
         return render_template("preview.html", img_url="/user_file/" + file_name)
     else:
         return redirect(url_for("home"))
+
+
+@app.route("/predict", methods=["GET", "POST"])
+def predict():
+    if request.method == "POST":
+        disease_type = request.form.get("disease-type")
+        print(disease_type)
+        user_file = "classifier/static/user_file"
+        file = os.listdir(os.path.join(os.getcwd(), user_file))
+        return render_template("predict.html", img_url="/user_file/" + file[0])
